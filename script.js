c4672d03089866ad847745638e6c5bec1c5ebb1e
TheRexous <144456224+TheRexous@users.noreply.github.com>
2024-03-19 18:06:25 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const bob = document.querySelector('.face')
+
+let count = 0
+
+function moveBob() {
+    console.log('hi')
+    count += 50
+    console.log(count)
+    bob.computedStyleMap.left = count + 'px'
+}
+bob.addEventListener('click', moveBob)
